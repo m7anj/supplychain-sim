e309fc8316e89476c1a2ad817c8575a7def822dd
Manjeev Takhar <manjeevt@hotmail.com>
2026-03-16 21:29:57 +0000

    feat(main routes): - set up a ping pong test route - created a bare bones /simulate endpoint in which the sim engine will be ran

--- server/main.py
+++ server/main.py
@@ -1,3 +1,18 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/ping")
+def pong():
+  return {"ping": "pong!"}
+
+@app.get("/simulate")
+def simulate():
+  # TODO: Implementing a /simulate entry point for the sim engine to be ran
+  return {"result": "this is a test"}
+
+
+
 def main():
     print("Hello from server!")
 
